c9c7424a086584fa6fcfa16d913974a135ce44a8
Franco Lanaro <franco.k.lanaro@gmail.com>
2025-11-30 11:52:30 -0300

    Use axios to fetch the data

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -1,8 +1,14 @@
+import axios from "axios";
 import Header from "../components/header.jsx";
 import { products } from "../../starting-code/data/products";
 import "./HomePage.css";
 
 export default function HomePage() {
+  axios.get("http://localhost:3000/api/products")
+    .then((response) => {
+      console.log(response.data);
+    })
+
   return (
     <>
       <title>Ecommerce Project</title>
